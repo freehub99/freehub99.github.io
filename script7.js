41134abb6af01a83b677b226c0c1f896a005ed42
freehub99 <t0766054+github@gmail.com>
2025-08-18 00:21:37 +0700

    Update script7.js

--- script7.js
+++ script7.js
@@ -17,7 +17,7 @@ const pageNumbers = document.getElementById('pageNumbers');
 
 const itemsPerPage = 20;
 let currentPage = 1;
-let filteredList = animeList.filter(anime => anime.type.includes("หีอมชมพู"));
+let filteredList = animeList.filter(anime => anime.type.includes("."));
 
 function renderPage(page) {
   animeGrid.innerHTML = '';
